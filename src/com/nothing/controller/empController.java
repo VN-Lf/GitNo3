3fccf84da3b03e137a0c8bf17e7f44fa779e9399
17870097767 <58416957+17870097767@users.noreply.github.com>
2019-12-04 21:27:47 +0800

    报修列表

---
+++ src/com/nothing/controller/empController.java
@@ -0,0 +1,9 @@
+package com.nothing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("emp")
+public class empController {
+}
