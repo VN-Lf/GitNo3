6d9de67a0301559b6e13d003230be51959ea4e5a
freedomnice <3218774798@qq.com>
2019-12-04 10:05:09 +0800

    考核更新

---
+++ src/com/nothing/service/Examservice.java
@@ -0,0 +1,4 @@
+package com.nothing.service;
+
+public interface Examservice {
+}
---
+++ src/com/nothing/service/impl/Examserviceimpl.java
@@ -0,0 +1,6 @@
+package com.nothing.service.impl;
+
+import com.nothing.service.Examservice;
+
+public class Examserviceimpl implements Examservice{
+}
