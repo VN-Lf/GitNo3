30101808a65b43f7e7a88fcb6a36fd7b7f251c9e
VN-Lf <zyqdyyzd@iCloud.com>
2019-12-08 19:29:57 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/nothing/controller/empController.java
    #	src/com/nothing/dao/BaseDao.java
    #	web/WEB-INF/jsp/emp/empadd.jsp
    #	web/WEB-INF/jsp/emp/emplist.jsp
    #	web/WEB-INF/static/layui/css/layui.css

--- src/com/nothing/controller/empController.java
+++ src/com/nothing/controller/empController.java
@@ -8,19 +8,13 @@ import com.nothing.vo.emp.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-//控制器
 @Controller
 @RequestMapping("emp")
 public class EmpController {
@@ -43,94 +37,20 @@ public class EmpController {
     @RequestMapping("/empadd")
     public void EmpList(Emp emp, EmpEducation empEducation, Post post, String ruzhitime, String birthday) throws ParseException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        if(!"".equals(birthday)){
-            emp.setEmpBirthday(simpleDateFormat.parse(birthday));
-        }
-        if(!"".equals(ruzhitime)){
-            emp.setEmpFireday(simpleDateFormat.parse(ruzhitime));
-        }
+        emp.setEmpBirthday(simpleDateFormat.parse(birthday));
+        emp.setEmpFireday(simpleDateFormat.parse(ruzhitime));
         System.out.println("员工：" + emp);
         System.out.println(ruzhitime + "学历：" + empEducation);
         System.out.println(birthday + "职务：" + post.getPostName());
         empService.addEmp(emp, empEducation, post);
     }
 
-    @RequestMapping("/toDel")
+    @RequestMapping("toDel")
+    @ResponseBody
     public String toDel(String id) {
         System.out.println("进来" + id);
         id = id.substring(0, id.length() - 1);
         empService.delete(id);
         return "成功";
     }
-
-    @RequestMapping("/toDelon")
-    public void toDelOn(String sid,HttpServletResponse resp) throws IOException {
-        System.out.println("进来" + sid);
-        PrintWriter pw = resp.getWriter();
-        empService.delete(sid);
-        pw.write("true");
-        pw.flush();
-        pw.close();
-    }
-
-    @RequestMapping("/czpwd")
-    public void czPwd(String sid,HttpServletResponse resp) throws IOException {
-        PrintWriter pw = resp.getWriter();
-        empService.czPwd(sid);
-        pw.write("true");
-        pw.flush();
-        pw.close();
-    }
-
-    @RequestMapping("/empup")
-    public String sqlEmp(String id,HttpServletRequest req){
-        Emp emp = empService.sqlEmpVo(id);
-        Post post = empService.sqlPostVo(id);
-        EmpEducation emdu = empService.sqlEduVo(id);
-        System.out.println("postName:"+post.getPostName());
-        req.setAttribute("emp",emp);
-        req.setAttribute("postvo",post);
-        req.setAttribute("emdutvo",emdu);
-        return  "emp/empupdate";
-    }
-    /*@RequestMapping("/empup")
-    public ModelAndView EmpUp(String id,ModelAndView mv){
-        Emp emp = empService.sqlEmpVo(id);
-        Post post = empService.sqlPostVo(id);
-        EmpEducation emdu = empService.sqlEduVo(id);
-        mv.addObject("empvo",emp);
-        mv.addObject("postvo",post);
-        mv.addObject("emdvo",emdu);
-        mv.setViewName("emp/empupdate");
-        System.out.println("major:"+emdu.getEmpEduMajor()+"school:"+emdu.getEmpUniversity());
-        return mv;
-    }*/
-
-    @RequestMapping("/update")
-    public void EmpUpdate(Emp emp, EmpEducation Edu, Post post, String ruzhitime, String birthday) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        if(!"".equals(birthday)){
-            emp.setEmpBirthday(simpleDateFormat.parse(birthday));
-        }
-        if(!"".equals(ruzhitime)){
-            emp.setEmpFireday(simpleDateFormat.parse(ruzhitime));
-        }
-        System.out.println("empid：" + emp.getEmpId()+"eduid:"+Edu.getEmpEduId()+"postid:"+post.getPostId());
-        empService.empUpdate(emp, Edu, post);
-    }
-
-
-    /*@RequestMapping({"/empEducationList"})
-    @ResponseBody
-    public JSONObject getEducationList(String eid) {
-        int id = Integer.parseInt(eid);
-        List eduList = this.empService.selEmpEducation(id);
-        this.empService.getEmpEducationCount(id);
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("code", 0);
-        jsonObject.put("msg", "");
-        jsonObject.put("data", eduList);
-        System.out.println(jsonObject.toJSONString());
-        return jsonObject;
-    }*/
 }
